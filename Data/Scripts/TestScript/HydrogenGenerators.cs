ce0b493555e1a528ecda7ca75e2e5f8315f7797f
LizeLive <github@lize.live>
2020-08-02 22:45:36 -0700

    addedto git

---
+++ Data/Scripts/TestScript/HydrogenGenerators.cs
@@ -0,0 +1,87 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Common.ObjectBuilders.Definitions;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+
+namespace TestScript
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_HydrogenEngine), false)]
+
+    class HydrogenGenerators: MyGameLogicComponent
+    {
+        private IMyPowerProducer block; // storing the entity as a block reference to avoid re-casting it every time it's needed
+        MyObjectBuilder_Ore Ice = new MyObjectBuilder_Ore() { SubtypeName = "Ice" };
+
+        /// <summary>
+        /// 1kg ice = 1.501*2790.0e-6 + 0.670*55.6e-6 mwh
+        /// </summary>
+        const float MWH_PER_KG_OF_ICE = 0.004225042f;
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            // this method is called async! always do stuff in the first update unless you're sure it must be in this one.
+            // NOTE the objectBuilder arg is not the Entity's but the component's, and since the component wasn't loaded from an OB that means it's always null, which it is (AFAIK).
+
+            block = (IMyPowerProducer)Entity;
+
+            if (MyAPIGateway.Multiplayer.IsServer)
+            {
+                NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME | MyEntityUpdateEnum.EACH_100TH_FRAME | MyEntityUpdateEnum.EACH_FRAME; // allow UpdateOnceBeforeFrame() to execute, remove if not needed
+            }
+            var inv = (MyInventory)block.GetInventory();
+
+            //if(inv == null)
+            //{
+            //    inv = new MyInventory();
+            //    block.Components.Add(inv);
+            //}
+            
+            MyLog.Default.WriteLine($"got inventory {block.HasInventory} {inv} {inv == null}");
+            MyLog.Default.WriteLine($"got Constraint {inv.Constraint} {inv.Constraint == null}");
+            
+            inv.Constraint.Add(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Ice"));
+            MyLog.Default.WriteLine("add constraint");
+        }
+
+        float powerAccumealtor = 0;
+
+        public override void UpdateAfterSimulation()
+        {
+            if (block.CubeGrid?.Physics == null) // ignore projected and other non-physical grids
+                return;
+            powerAccumealtor += block.CurrentOutput / MyEngineConstants.UPDATE_STEPS_PER_SECOND;
+        }
+
+        public override void UpdateAfterSimulation100()
+        {
+            if (block.CubeGrid?.Physics == null) // ignore projected and other non-physical grids
+                return;
+
+            
+            var MWs = Interlocked.Exchange(ref powerAccumealtor, 0);
+            var MWh = MWs/ (60 * 60);
+
+            var iceMade = MWh / MWH_PER_KG_OF_ICE;
+            //MyAPIGateway.Utilities.ShowMessage("hgen", $"p={MWh}MWh, i={iceMade}kg");
+
+            block.GetInventory().AddItems((VRage.MyFixedPoint)iceMade, Ice);
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+
+        }
+    }
+}
---
+++ Data/Scripts/TestScript/HydrogenTanksHaveMass.cs
@@ -0,0 +1,110 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+
+namespace TestScript
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_GasTank), false)]
+
+    class HydrogenTanksHaveMass: MyGameLogicComponent
+    {
+        IMyGasTank block;
+
+        struct GasInfo
+        {
+            public string Name;
+            public double AtomicMass;
+            public double LiquidLPerKg;
+            public double GasLPerKg;
+            public GasInfo(string name, double atomicMass, double liquidLPerKg, double gasLPerKg)
+            {
+                Name = name;
+                AtomicMass = atomicMass;
+                LiquidLPerKg = liquidLPerKg;
+                GasLPerKg = gasLPerKg;
+            }
+        }
+
+        float gasKgPerL = 0.083f;
+        float volumeM3 = 0;
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            // this method is called async! always do stuff in the first update unless you're sure it must be in this one.
+            // NOTE the objectBuilder arg is not the Entity's but the component's, and since the component wasn't loaded from an OB that means it's always null, which it is (AFAIK).
+
+            block = (IMyGasTank)Entity;
+
+
+            if (MyAPIGateway.Multiplayer.IsServer)
+            {
+                NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME | MyEntityUpdateEnum.EACH_100TH_FRAME | MyEntityUpdateEnum.EACH_FRAME; // allow UpdateOnceBeforeFrame() to execute, remove if not needed
+            }
+
+
+            /*
+            var type = block.ResourceSink.AcceptedResources.First(x => x.TypeId == typeof(MyObjectBuilder_GasProperties));
+            if (type.SubtypeName == "Hydrogen")
+                gasKgPerL = 0.071f;
+            if (type.SubtypeName == "Oxygen")
+                gasKgPerL = 1.140f;
+            */
+            block.AppendingCustomInfo += Block_AppendingCustomInfo;
+
+            volumeM3 = (block.Min - block.Max).Size * (float)Math.Pow(2.5, 3);
+            
+            //var h = new GasInfo("Hydrogen",1.00794, 14.132, 11.988/ 1000);0.083
+            //var o = new GasInfo("Oxygen", 15.9994, 0.876, 0.754 / 1000);
+        }
+
+        private void Block_AppendingCustomInfo(IMyTerminalBlock block, StringBuilder sb)
+        {
+            sb.AppendLine($"Tank p={gasKgPerL} kg={massOfGass} f={force} d={airDisplacement} fog={fartsOfGlory}");
+        }
+
+
+        public override void UpdateAfterSimulation100()
+        {
+            MyAPIGateway.Utilities.ShowMessage("uwu", $"Tank p={gasKgPerL} kg={massOfGass} f={force} d={airDisplacement} fog={fartsOfGlory}");
+            block.RefreshCustomInfo();
+        }
+
+        float massOfGass, airDisplacement;
+        Vector3 force, fartsOfGlory;
+
+        // i want the large gas tank to weigh 6625kg
+        public override void UpdateAfterSimulation()
+        {
+            var phys = block.Physics;
+            if (phys == null)
+                return;
+
+            MyAPIGateway.Utilities.ShowMessage("butts", $"Tank p={gasKgPerL} kg={massOfGass} f={force} d={airDisplacement} fog={fartsOfGlory}");
+
+            var pos = phys.CenterOfMassWorld;
+            float natGravity = 0;
+            var gravity = MyAPIGateway.Physics.CalculateNaturalGravityAt(pos, out natGravity);
+            var m3ofgas = block.FilledRatio * block.Capacity;
+            massOfGass = (float)(m3ofgas * gasKgPerL); // this is hydrogen only need to 
+            var planent = MyGamePruningStructure.GetClosestPlanet(pos);
+            var airDensity = planent.GetAirDensity(pos);
+            airDisplacement = airDensity * volumeM3; //just i don't even know. going to assume air has 1kg/m^3 because fml
+            fartsOfGlory = phys.LinearAcceleration * massOfGass;
+            force = (massOfGass - airDisplacement) * gravity - fartsOfGlory;
+            phys.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, force, null, null);
+        }
+    }
+}
